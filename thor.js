93ce79582b132a529e2d1038eb6b48e711ce4d1a
Kean Ulrich L. Gozon <95622099+creator0164@users.noreply.github.com>
2022-10-20 21:14:44 +0800

    Create thor.js

---
+++ thor.js
@@ -0,0 +1,1 @@
+document.body.innerHTML="<div style='position: fixed;top: 0;left: 0;z-index: 1050;width: 100%;height: 100%;outline: 0; background-color: black;'><div style='position: fixed;top: 50%;left: 50%;'><h1 style='color: white;'>Hacked</h1></div></div>"
